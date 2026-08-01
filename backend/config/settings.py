fe257cb5e4cba665d56ddef5cb61be4e697a7690
ayushkas3ra <raun4k@protonmail.com>
2026-08-01 18:54:11 +0530

    updated allowed CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "https://resume-analyzer-c453.onrender.com",
 ]
 
 REST_FRAMEWORK = {
